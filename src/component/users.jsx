f47b7ee152c0ce2337428dd2b481ec3ac78b30e3
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-05-14 18:09:56 +0600

    get data from data base and use them

--- src/component/users.jsx
+++ src/component/users.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const users = () => {
+
+    const users=useLoaderData()
+
     return (
         <div>
-            <h1>all users here</h1>
+            <h1>all users here {users.length}</h1>
+            {
+                users.map(user=><p key={user._id}>{user.name} : {user.email}</p> )
+            }
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Users from './component/Users.jsx'
 import './index.css'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path:"/users",
+    element:<Users />,
+    loader: ()=>fetch("http://localhost:5000/users")
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
