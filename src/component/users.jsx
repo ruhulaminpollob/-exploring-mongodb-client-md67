ca400bb1d7c85b4c4313abce5330cefde4711e1b
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-05-15 20:26:47 +0600

    added delete data from database function

--- src/component/users.jsx
+++ src/component/users.jsx
@@ -5,11 +5,15 @@ const users = () => {
 
     const users=useLoaderData()
 
+    const handleDelete=id=>{
+        console.log(id);
+    }
+
     return (
         <div>
             <h1>all users here {users.length}</h1>
             {
-                users.map(user=><p key={user._id}>{user.name} : {user.email}</p> )
+                users.map(user=><p key={user._id}>{user.name} : {user.email} {user._id} <button onClick={()=>handleDelete(user._id)}>X</button> </p> )
             }
         </div>
     );
